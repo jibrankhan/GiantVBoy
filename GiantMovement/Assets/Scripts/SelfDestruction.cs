a5f8381186819c2199d55577544bed8f8c208f2b
mercurian-h <nicky.hsj@gmail.com>
2016-09-18 23:32:22 -0400

    Change Lightning Prefab

--- GiantMovement/Assets/Scripts/SelfDestruction.cs
+++ GiantMovement/Assets/Scripts/SelfDestruction.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class SelfDestruction : MonoBehaviour {
 
-    public int timer = 10000;
+    public int timer = 24;
 
 	// Use this for initialization
 	void Start () {
