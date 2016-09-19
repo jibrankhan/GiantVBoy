c5964137aa09d678d44be24fd4f8c4a06046442e
mercurian-h <nicky.hsj@gmail.com>
2016-09-18 23:13:01 -0400

    Add lightning

---
+++ GiantMovement/Assets/Scripts/SelfDestruction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestruction : MonoBehaviour {
+
+    public int timer = 10000;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(timer >= 0)
+        {
+            timer--;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+}
